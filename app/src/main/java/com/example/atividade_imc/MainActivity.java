1b8fdf172ac0c8878f2a75d782d7d1e802d6f2be
AdrielMassagliAraujo <102391199+AdrielMassagliAraujo@users.noreply.github.com>
2022-06-24 12:06:18 -0300

    commit2

--- app/src/main/java/com/example/atividade_imc/MainActivity.java
+++ app/src/main/java/com/example/atividade_imc/MainActivity.java
@@ -17,20 +17,64 @@ public class MainActivity extends AppCompatActivity {
         txt1 = findViewById(R.id.textalt);
         txt2 = findViewById(R.id.textpeso);
         txt3 = findViewById(R.id.textresult);
+        txt4 = findViewById(R.id.textresult2);
     }
 
     EditText txt1, txt2;
-    TextView txt3;
+    TextView txt3, txt4;
 
-    int val1, val2;
+    float val1;
+    int val2;
 
 
     public void Calcular(View view){
-        val1 = Integer.parseInt(txt1.getText().toString());
+        val1 = Float.parseFloat(txt1.getText().toString());
         val2 = Integer.parseInt(txt2.getText().toString());
 
-        int cal = val2 / (val1 * val1);
+        float calc = val2 / (val1 * val1);
 
-        txt3.setText(Integer.valueOf(cal).toString());
+        txt3.setText(Float.valueOf(calc).toString());
+
+        if(calc > 43){
+            txt4.setText("Obesidade Mórbida");
+        }
+        else if(calc > 30){
+            txt4.setText("Obesidade Moderada");
+        }
+        else if(calc > 25){
+            txt4.setText("Obesidade Leve");
+        }
+        else if(calc > 20){
+            txt4.setText("Normal");
+        }
+        else if(calc < 20){
+            txt4.setText("Abaixo do Normal");
+        }
     }
+
+    public void Calcular2 (View view){
+        val1 = Float.parseFloat(txt1.getText().toString());
+        val2 = Integer.parseInt(txt2.getText().toString());
+
+        float calc = val2 / (val1 * val1);
+
+        txt3.setText(Float.valueOf(calc).toString());
+
+        if(calc > 39){
+            txt4.setText("Obesidade Mórbida");
+        }
+        else if(calc > 29){
+            txt4.setText("Obesidade Moderada");
+        }
+        else if(calc > 24){
+            txt4.setText("Obesidade Leve");
+        }
+        else if(calc > 19){
+            txt4.setText("Normal");
+        }
+        else if(calc < 19){
+            txt4.setText("Abaixo do Normal");
+        }
+    }
+
 }
